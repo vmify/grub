3beb09779f28f1b193c9ff215525dd847fc62874
Axel Fontaine <axel@boxfuse.com>
2022-07-31 17:47:23 +0200

    Fixed efi file name

--- build.sh
+++ build.sh
@@ -8,15 +8,17 @@ echo "insmod gzio" >> grub.cfg
 echo "set root=(hd0,gpt1)" >> grub.cfg
 if [ "$ARCH" = "x64" ]; then
   format=x86_64-efi
+  output=bootx64.efi
 else
   format=arm64-efi
+  output=bootaa64.efi
 fi
-grub-mkimage -c grub.cfg -O $format -o grub.efi -p /EFI/BOOT normal part_gpt fat linux gzio
+grub-mkimage -c grub.cfg -O $format -o $output -p /EFI/BOOT normal part_gpt fat linux gzio
 
 echo Packaging GRUB ...
 mkdir /export
 cd /export || exit
-cp /build/grub.efi .
+cp /build/$output .
 cp /build/LICENSE .
 echo "https://git.alpinelinux.org/aports/tree/main/grub?id=1895cf9fc22dde29d848d62c20eb0276ea2d34a7" > /export/SOURCE
 echo "2.06-r2" > /export/VERSION
